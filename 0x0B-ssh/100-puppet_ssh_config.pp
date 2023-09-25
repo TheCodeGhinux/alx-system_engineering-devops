da3f218b9e38aea9b0207ff6e76aeeba4ff63e75
Ayomide <ilesanmibry@gmail.com>
2023-09-24 20:15:41 -0700

    commit

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#  practice using Puppet to make changes to our configuration file
+
+#Puupet to configure SSH clint
+file_line { 'Turn off passwd auth':
+	path => '/etc/ssh/ssd_config',
+	line => 'PasswordAuthentication no',
+	ensure => present,
+ }
+
+file_line { 'Declare identity file':
+	path => '/etc/ssh/ssh_config',
+	line => 'IdentityFile `/.ssh/school',
+	ensure => present,
+}
