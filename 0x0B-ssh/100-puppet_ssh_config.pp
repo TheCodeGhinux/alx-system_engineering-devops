bbd3ccecaef4f388908ebb15beca7bf27b5d7caf
Ayomide <ilesanmibry@gmail.com>
2023-11-27 01:12:37 +0000

    commit 4-4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # using Puppet to make changes to our configuration file
 file_line { 'no password':
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
-  ensure => present,
 }
 file_line { 'change private key':
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school'
-  ensure => present,
 }
