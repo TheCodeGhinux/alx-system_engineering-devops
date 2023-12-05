01598d04aac7c65138b59bc08181935b45104fcc
Ayomide <ilesanmibry@gmail.com>
2023-12-05 06:31:00 +0000

    commit 1

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 # create a manifest that kills a process  named killmenow
-
-exec {'pkill':
-  command => 'pkill -f killmenow',
-  path    => '/usr/bin'
+exec { 'pkill':
+  command => 'pkill -9 -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
 }
