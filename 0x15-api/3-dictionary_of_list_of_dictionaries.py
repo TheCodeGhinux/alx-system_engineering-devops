aac64c8031b55dddd30cad4d6507c8a5c3675a3a
Ayomide <ilesanmibry@gmail.com>
2024-01-03 09:36:51 +0000

    commit 1-5

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
                 "task": task.get("title"),
                 "completed": task.get("completed"),
                 "username": user.get("username")
-            } for task in requests.get(base_url + "todos",
-                                       params={"userId":
-                                       user.get("id")}).json()]
+            } for task in requests.get
+                             (base_url + "todos", params=
+                              {"userId": user.get("id")}).json()]
             for user in users}, jsonfile)
