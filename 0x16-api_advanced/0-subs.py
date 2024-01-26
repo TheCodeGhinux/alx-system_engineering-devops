b67d04bec1530cca26b42c0d3918eb3a6ebf67db
Ayomide <ilesanmibry@gmail.com>
2024-01-26 03:47:19 -0700

    commit 2-3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "advanced.api/1.0 (by /u/codeghinux)"}
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/codeghinux)"}
 
     response = requests.get(url, headers=headers,
                             allow_redirects=False)
