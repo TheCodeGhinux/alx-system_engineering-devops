37cc4753511355c1830dc83b9a58b4d8e9ca2412
Ayomide <ilesanmibry@gmail.com>
2024-01-26 03:24:25 -0700

    commit 2-3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,9 +12,23 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "advanced.api/1.0"}
 
-    response = requests.get(url, headers=headers,
-                            allow_redirects=False)
-    if response.status_code == 404:
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raise an exception for bad responses
+
+        # Check if there is a redirect and follow it
+        if response.is_redirect:
+            redirected_url = response.headers['Location']
+            response = requests.get(redirected_url, headers=headers)
+            response.raise_for_status()
+
+        results = response.json().get("data")
+        return results.get("subscribers")
+    except requests.exceptions.HTTPError as e:
+        if e.response.status_code == 404:
+            return 0
+        else:
+            print(f"Error: {e}")
+    except Exception as e:
+        print(f"Exception: {e}")
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
