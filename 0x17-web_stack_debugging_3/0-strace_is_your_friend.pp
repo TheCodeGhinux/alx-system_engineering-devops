4609d788804fbe9036e94dba913ada936e034480
Ayomide <ilesanmibry@gmail.com>
2024-02-02 07:41:11 +0100

    commit 1-2

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # Fixes error in apache using puppet
 
+
 exec { 'fix-wordpress':
-  command   => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path      => '/usr/local/bin/:/bin/',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
