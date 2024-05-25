d0f3d400c90805c5dc19383867d21a2b7b7c725f
Ayomide <ilesanmibry@gmail.com>
2024-05-25 20:48:29 +0100

    fix

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -46,3 +46,4 @@ def count_words(subreddit, word_list, after=None, counts=None):
                                key=lambda x: (-x[1], x[0].lower()))
         for word, count in sorted_counts:
             print("{}: {}".format(word.lower(), count))
+        return "OK"
