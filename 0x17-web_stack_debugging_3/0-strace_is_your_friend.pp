869d4f0970bc942c1818f65494cc0037b027f7c0
Ayomide <ilesanmibry@gmail.com>
2024-05-25 19:59:32 +0100

    fix error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,1 +1,6 @@
-1
+# used strace to fix error in wordpress automatically
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
