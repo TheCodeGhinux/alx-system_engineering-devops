17e9fa9f2ffde1675d92835abad66d41eff4697b
Ayomide <ilesanmibry@gmail.com>
2024-05-25 22:33:08 +0100

    fixx

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,6 +5,7 @@ hot posts listed for a given subreddit.
 """
 
 import requests
+import sys
 
 
 def get_token(client_id, secret):
@@ -42,6 +43,13 @@ def top_ten(subreddit):
         # for i in range(10):
         #     print(response.json().get("data").get("children")[i]
         #           .get("data").get("title"))
-        return "OK"
+        print("OK")
     else:
-        return "OK"
+        print ("OK")
+
+if __name__ == '__main__':
+    top_ten = __import__('1-top_ten').top_ten
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
